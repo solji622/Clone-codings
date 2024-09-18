27ea49bf045ddebb7e430a0f0f3f648a9037f991
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2024-09-18 20:45:10 +0900

    Add files via upload

--- Vanilla JS For Momentum/app.js
+++ Vanilla JS For Momentum/app.js
@@ -1,11 +1,21 @@
-const loginForm = document.getElementById("login-form");
+const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
-// loginForm에서 html의 #iogin-form을 찾고 input, btn을 차례대로 찾아냄
+// loginForm에서 html의 #iogin-form을 찾고 input을 차례대로 찾아냄
+const greeting = document.querySelector("#greeting");
 
-function onLoginDtnClick() {
+const HIDDEN_CLASSNAME = "hidden";
+// String만 포함된 변수는 대문자 표기, string 저장하고 싶을 때 사용
+
+function onLoginSubmit(event) {
+    event.preventDefault();
     const username = loginInput.value;
-    console.log(username);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem("username",  username)
 }
 
-loginButton.addEventListener("click", onLoginDtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+
+
+
