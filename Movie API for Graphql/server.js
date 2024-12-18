fd228bb4a6ba8beefa39a89dd49ddb5b000513a7
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2024-12-18 14:16:14 +0900

    Add files via upload

--- Movie API for Graphql/server.js
+++ Movie API for Graphql/server.js
@@ -2,9 +2,25 @@ import {ApolloServer, gql} from "apollo-server";
 
 // 따옴표 아닌 백틱 사용
 const typeDefs = gql`
-    type Query {
-        text: String
-        hello: String
+    type User {
+        id:ID!
+        username: String!
+        firstName: String!
+        lastName: String!
+    }
+    type Tweet {
+        id:ID!
+        text:String!
+        author: User!
+    }
+    type Query { # query에 넣은 모든 필드들은 user에 의해 request 됨
+        allTweets: [Tweet!]!
+        tweet(id: ID!): Tweet!
+        # user가 어떤 Tweet을 말하는 것인지 모름! > 아규먼트 지정
+    }
+    type Mutation {
+        postTweet(text: String!, userId: ID!): Tweet!
+        deleteTweet(id:ID!): Boolean #트윗 삭제 시 트윗 있으면 true, 없으면 false 리턴
     }
 `;
 
