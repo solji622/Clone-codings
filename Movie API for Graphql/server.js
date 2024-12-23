41b735d48b2a1f8a7cd696dbfb08d707fa6b684b
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2024-12-23 14:59:48 +0900

    Add files via upload

--- Movie API for Graphql/server.js
+++ Movie API for Graphql/server.js
@@ -1,22 +1,46 @@
 import {ApolloServer, gql} from "apollo-server";
 
 // 따옴표 아닌 백틱 사용
+let tweets = [
+    {
+        id:"1",
+        text:"first one!",
+    },
+    {
+        id:"2",
+        text:"second one",
+    },
+]
+
+let users = [
+    {
+        id: "1",
+        firstName: "solji",
+        lastName: "Lee",
+    },
+    {
+        id:"2",
+        firstName: "sungwoo",
+        lastName: "kim",
+    },
+]
+
 const typeDefs = gql`
     type User {
         id:ID!
-        username: String!
         firstName: String!
         lastName: String!
+        fullName: String!
     }
     type Tweet {
         id:ID!
         text:String!
-        author: User!
+        author: User
     }
     type Query { # query에 넣은 모든 필드들은 user에 의해 request 됨
+        allUsers: [User!]!
         allTweets: [Tweet!]!
         tweet(id: ID!): Tweet
-        ping: String!
         # user가 어떤 Tweet을 말하는 것인지 모름! > 아규먼트 지정
     }
     type Mutation {
@@ -27,13 +51,39 @@ const typeDefs = gql`
 
 const resolvers = {
     Query: {
-        tweet() {
-            console.log("I'm called"); 
-            return null;
+        allTweets() {
+            return tweets;
         },
-        ping() {
-            return "pong";
+        tweet(root, {id}) {
+            return tweets.find((tweet) => tweet.id === id)
         },
+        allUsers() {
+            console.log("allUsers called!")
+            return users;
+        },
+    },
+    Mutation: {
+        postTweet(_, {text, userId}) {
+            const newTweet = {
+                id: tweets.length + 1,
+                text,
+            };
+            tweets.push(newTweet);
+            return newTweet;
+        },
+
+        deleteTweet(_, {id}) {
+            const tweet = tweets.find(tweet => tweet.id === id);
+            if (!tweet) return false;
+            tweets = tweets.filter(tweet => tweet.id !== id) //tweet db array를 정리하는 시간
+            // tweet의 id가 삭제하려는 id와 같지 않은 tweet들로 filter를 거치면 array 반환 > tweets let으로 변환
+            return true;
+        },
+    },
+    User: {
+        fullName({firstName, lastName}) {
+            return `${firstName} ${lastName}`;
+        }
     },
 };
 
